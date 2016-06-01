3d464cc3c83855ffa0418bc01b354c75dee3680f
CG <heartlandergames@gmail.com>
2016-05-31 21:00:48 -0500

    ColorManager
    Colors on the mainscreen change randomly

---
+++ LoseYourMarbles/Assets/Scripts/ColorManager.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+import System.Collections.Generic;
+import UnityEngine.UI;
+
+var colorList : List.<Color> = new List.<Color>();
+
+var mainList : List.<Color> = new List.<Color>();
+
+var accentList : List.<Color> = new List.<Color>();
+
+
+function Start () {
+    var choice : int = Random.Range(0, mainList.Count-1);
+
+   
+
+    Camera.main.backgroundColor = mainList[choice];
+
+    var buttonObjectArray : GameObject[] = GameObject.FindGameObjectsWithTag("BUTTON");
+    for(var b : GameObject in buttonObjectArray)
+    {
+        b.GetComponent(Image).color = accentList[choice];
+        b.GetComponent(Image).color.a = 255;
+    }
+
+}
+
+function Update () {
+
+}
